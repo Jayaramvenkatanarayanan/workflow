ca700dc3a2e262366b939bac792c9c75ab5a11bc
Jayaram <jayaramvenkatanarayanan@live.com>
2024-10-01 07:11:44 +0530

    condition modified

--- .github/actions/docker-ping/entrypoint.sh
+++ .github/actions/docker-ping/entrypoint.sh
@@ -1,16 +1,20 @@
+#!/bin/sh
+
 set -e
 
-URL="$1"
-MAX_TRIALS="$2"
-DELAY="$3"
+# Get inputs from environment variables
+URL="${INPUT_URL}"
+MAX_TRIALS="${INPUT_MAX_TRIALS}"
+DELAY="${INPUT_DELAY}"
 
-for i in $(seq 1 $MAX_TRIALS); do
+# Loop to ping the URL the specified number of times
+for i in $(seq 1 "$MAX_TRIALS"); do
   if curl --silent --fail "$URL"; then
     echo "Ping to $URL succeeded on trial $i"
     exit 0
   else
     echo "Ping to $URL failed on trial $i"
-    sleep $DELAY
+    sleep "$DELAY"
   fi
 done
 
